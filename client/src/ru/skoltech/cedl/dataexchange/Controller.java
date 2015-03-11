7275e45cb9a8bdd76d64f947d248ce8ca239b60d
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-11 13:23:13 +0300

    designed the principal scene for editing the study

---
+++ client/src/ru/skoltech/cedl/dataexchange/Controller.java
@@ -0,0 +1,14 @@
+package ru.skoltech.cedl.dataexchange;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
+
+public class Controller {
+
+    @FXML
+    private TreeView<?> structureTree;
+
+    @FXML
+    private TableView<?> nodePropertyTable;
+}
---
+++ client/src/ru/skoltech/cedl/dataexchange/Main.java
@@ -0,0 +1,23 @@
+package ru.skoltech.cedl.dataexchange;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("study-editing.fxml"));
+        primaryStage.setTitle("Concurrent Engineering Data Exchange Skoltech");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
