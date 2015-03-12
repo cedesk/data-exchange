1bba196a4b90ebf76986bf7567eabff296f92dce
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-12 11:43:36 +0300

    added basic model hiarchy

---
+++ client/src/ru/skoltech/cedl/dataexchange/structure/model/Element.java
@@ -0,0 +1,10 @@
+package ru.skoltech.cedl.dataexchange.structure.model;
+
+/**
+ * Created by D.Knoll on 11.03.2015.
+ */
+public class Element extends ModelNode {
+    public Element(String name) {
+        super(name);
+    }
+}
