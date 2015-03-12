86962779cd2acd69b5020f798cdb866da8dd5cc2
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-12 15:45:32 +0300

    refactored Views, improved click handler for treeView

--- client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewNode.java
+++ client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewNode.java
@@ -4,39 +4,39 @@ import javafx.scene.Node;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import ru.skoltech.cedl.dataexchange.structure.model.ElementModel;
-import ru.skoltech.cedl.dataexchange.structure.model.InstrumentModel;
-import ru.skoltech.cedl.dataexchange.structure.model.SubSystemModel;
-import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
+import ru.skoltech.cedl.dataexchange.Utils;
+import ru.skoltech.cedl.dataexchange.structure.model.*;
+
+import java.io.InputStream;
 
 /**
  * Created by D.Knoll on 12.03.2015.
  */
-public class ViewNode extends TreeItem<String> {
+public class ViewNode extends TreeItem<ModelNode> {
 
-    final Node sysIcon = new ImageView(new Image(this.getClass().getResourceAsStream("../../../../../../spacecraft.png")));
+    final Node sysIcon = new ImageView(new Image(Utils.getResourceAsStream("spacecraft.png")));
 
-    final Node subsysIcon = new ImageView(new Image(getClass().getResourceAsStream("../../../../../../subsystem.png")));
+    final Node subsysIcon = new ImageView(new Image(Utils.getResourceAsStream("subsystem.png")));
 
-    final Node elementIcon = new ImageView(new Image(getClass().getResourceAsStream("../../../../../../element.png")));
+    final Node elementIcon = new ImageView(new Image(Utils.getResourceAsStream("element.png")));
 
     public ViewNode(SystemModel system) {
-        super(system.getName());
+        super(system);
         super.setGraphic(sysIcon);
     }
 
     public ViewNode(SubSystemModel subSystemModel) {
-        super(subSystemModel.getName());
+        super(subSystemModel);
         super.setGraphic(subsysIcon);
     }
 
     public ViewNode(ElementModel elementModel){
-        super(elementModel.getName());
+        super(elementModel);
         super.setGraphic(elementIcon);
     }
 
     public ViewNode(InstrumentModel instrumentModel){
-        super(instrumentModel.getName());
+        super(instrumentModel);
     }
 
 }
