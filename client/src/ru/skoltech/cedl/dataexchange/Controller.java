c9ea39a23bff8921ccc924880756b81cf2517730
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-13 16:45:00 +0300

    enabled refresh of parameter view on node selection

--- client/src/ru/skoltech/cedl/dataexchange/Controller.java
+++ client/src/ru/skoltech/cedl/dataexchange/Controller.java
@@ -2,19 +2,22 @@ package ru.skoltech.cedl.dataexchange;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeView;
-import ru.skoltech.cedl.dataexchange.structure.model.DummySystemBuilder;
-import ru.skoltech.cedl.dataexchange.structure.model.ModelNode;
-import ru.skoltech.cedl.dataexchange.structure.model.ParameterModel;
-import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import ru.skoltech.cedl.dataexchange.structure.model.*;
 import ru.skoltech.cedl.dataexchange.structure.view.ViewNode;
 import ru.skoltech.cedl.dataexchange.structure.view.ViewTreeFactory;
 
+import java.util.List;
+
 public class Controller {
+
+    @FXML
+    public Button openButton;
 
     @FXML
     private TreeView<ModelNode> structureTree;
@@ -22,8 +25,12 @@ public class Controller {
     @FXML
     private TableView<ParameterModel> parameterTable;
 
+    public void setParameterTable(TableView<ParameterModel> parameterTable) {
+        this.parameterTable = parameterTable;
+    }
 
     public void loadTree(ActionEvent actionEvent) {
+        openButton.setDisable(true);
         SystemModel system = DummySystemBuilder.getSystemModel(2);
 
         ViewNode rootNode = ViewTreeFactory.getViewTree(system);
@@ -32,9 +39,15 @@ public class Controller {
                 .addListener(new ChangeListener<TreeItem<ModelNode>>() {
                     @Override
                     public void changed(ObservableValue<? extends TreeItem<ModelNode>> observable, TreeItem<ModelNode> oldValue, TreeItem<ModelNode> newValue) {
-                        System.out.println(newValue.getValue().getName());
+                        Controller.this.displayParameters(newValue.getValue());
                     }
                 });
+    }
+
+    private void displayParameters(ModelNode modelNode) {
+        ObservableList<ParameterModel> data =
+                FXCollections.observableArrayList(modelNode.getParameters());
+        parameterTable.setItems(data);
     }
 
     public void saveTree(ActionEvent actionEvent) {
--- client/src/ru/skoltech/cedl/dataexchange/structure/model/ParameterModel.java
+++ client/src/ru/skoltech/cedl/dataexchange/structure/model/ParameterModel.java
@@ -28,4 +28,44 @@ public class ParameterModel {
         this.isShared = isShared;
         this.description = description;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public ParameterType getType() {
+        return type;
+    }
+
+    public void setType(ParameterType type) {
+        this.type = type;
+    }
+
+    public Boolean getIsShared() {
+        return isShared;
+    }
+
+    public void setIsShared(Boolean isShared) {
+        this.isShared = isShared;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
