0408b814de0534e13c5f5757627becde589b1759
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-17 12:45:48 +0300

    removed incorrect check, added another check

--- client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewTreeFactory.java
+++ client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewTreeFactory.java
@@ -38,14 +38,12 @@ public class ViewTreeFactory {
     private static ViewNode getViewTree(ElementModel elementModel) {
         ViewNode node = new ViewNode(elementModel);
         node.setExpanded(true);
-        if (elementModel.getInstruments() == null ||
-                !(elementModel.getInstruments().isEmpty())) {
-            return node;
-        }
         for (Iterator<InstrumentModel> iter = elementModel.iterator(); iter.hasNext(); ) {
             InstrumentModel instrument = iter.next();
-            ViewNode subNode = new ViewNode(instrument);
-            node.getChildren().add(subNode);
+            ViewNode childNode = new ViewNode(instrument);
+            if (childNode != null) {
+                node.getChildren().add(childNode);
+            }
         }
         return node;
     }
