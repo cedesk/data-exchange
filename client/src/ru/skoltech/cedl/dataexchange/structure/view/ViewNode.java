bf28bfa95b25293ce1bc6959790ed2fc56387413
Dominik Knoll <d.knoll@skoltech.ru>
2015-03-28 19:11:21 +0300

    moved resources to a proper place

--- client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewNode.java
+++ client/src/ru/skoltech/cedl/dataexchange/structure/view/ViewNode.java
@@ -7,18 +7,16 @@ import javafx.scene.image.ImageView;
 import ru.skoltech.cedl.dataexchange.Utils;
 import ru.skoltech.cedl.dataexchange.structure.model.*;
 
-import java.io.InputStream;
-
 /**
  * Created by D.Knoll on 12.03.2015.
  */
 public class ViewNode extends TreeItem<ModelNode> {
 
-    final Node sysIcon = new ImageView(new Image(Utils.getResourceAsStream("spacecraft.png")));
+    final Node sysIcon = new ImageView(new Image(Utils.getResourceAsStream("resources/spacecraft.png")));
 
-    final Node subsysIcon = new ImageView(new Image(Utils.getResourceAsStream("subsystem.png")));
+    final Node subsysIcon = new ImageView(new Image(Utils.getResourceAsStream("resources/subsystem.png")));
 
-    final Node elementIcon = new ImageView(new Image(Utils.getResourceAsStream("element.png")));
+    final Node elementIcon = new ImageView(new Image(Utils.getResourceAsStream("resources/element.png")));
 
     public ViewNode(SystemModel system) {
         super(system);
