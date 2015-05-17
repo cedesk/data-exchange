bb2024b106edb2a6618ef97d5bfd45e3ecf528f7
Dominik Knoll <d.knoll@skoltech.ru>
2015-05-18 00:27:47 +0100

    added possibility to add user

--- client/src/main/java/ru/skoltech/cedl/dataexchange/controller/UserManagementController.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/controller/UserManagementController.java
@@ -9,12 +9,16 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.util.Callback;
+import ru.skoltech.cedl.dataexchange.Identifiers;
+import ru.skoltech.cedl.dataexchange.StatusLogger;
 import ru.skoltech.cedl.dataexchange.structure.Project;
 import ru.skoltech.cedl.dataexchange.users.model.Discipline;
 import ru.skoltech.cedl.dataexchange.users.model.User;
+import ru.skoltech.cedl.dataexchange.users.model.UserManagement;
 
 import java.net.URL;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -25,11 +29,33 @@ public class UserManagementController implements Initializable {
     public TableView disciplinesTable;
     public TableColumn userNameColumn;
     private Project project;
+    private UserManagement userManagement;
 
     public void addUser(ActionEvent actionEvent) {
+        Optional<String> userNameChoice = Dialogues.inputUserName();
+        if (userNameChoice.isPresent()) {
+            String userName = userNameChoice.get();
+            if (!Identifiers.validateNodeName(userName)) {
+                Dialogues.showError("Invalid name", Identifiers.getNameValidationDescription());
+                return;
+            }
+            if (project.getUserManagement().getDisciplineMap().containsKey(userName)) {
+                Dialogues.showError("Duplicate user name", "There is already a user named like that!");
+            } else {
+                // TODO: use factory
+                User user = new User();
+                user.setUserName(userName);
+                StatusLogger.getInstance().log("added user: " + user.getUserName());
+                userManagement.getUsers().add(user);
+            }
+        }
+        updateUserTable();
     }
 
     public void deleteUser(ActionEvent actionEvent) {
+        Object selecteditem = userTable.getSelectionModel().getSelectedItem();
+        userManagement.getUsers().remove(selecteditem);
+        updateUserTable();
     }
 
     public void addDiscipline(ActionEvent actionEvent) {
@@ -40,15 +66,19 @@ public class UserManagementController implements Initializable {
 
     public void setProject(Project project) {
         this.project = project;
-        updateTables();
+        this.userManagement = project.getUserManagement();
+        updateUserTable();
+        updateDisciplineTable();
     }
 
-    private void updateTables() {
-        List<User> users = project.getUserManagement().getUsers();
+    private void updateUserTable() {
+        List<User> users = userManagement.getUsers();
         ObservableList<User> userList = FXCollections.observableList(users);
         userTable.setItems(userList);
+    }
 
-        List<Discipline> disciplines = project.getUserManagement().getDisciplines();
+    private void updateDisciplineTable() {
+        List<Discipline> disciplines = userManagement.getDisciplines();
         ObservableList<Discipline> disciplineList = FXCollections.observableList(disciplines);
         disciplinesTable.setItems(disciplineList);
     }
