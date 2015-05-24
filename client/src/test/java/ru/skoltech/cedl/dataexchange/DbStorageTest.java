a6fc06b374339c9bd963233e1829d8dcde52a430
Dominik Knoll <d.knoll@skoltech.ru>
2015-05-24 12:49:39 +0300

    made DatabaseStorage similar to FileStorage

--- client/src/test/java/ru/skoltech/cedl/dataexchange/DbStorageTest.java
+++ client/src/test/java/ru/skoltech/cedl/dataexchange/DbStorageTest.java
@@ -3,7 +3,7 @@ package ru.skoltech.cedl.dataexchange;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import ru.skoltech.cedl.dataexchange.repository.DBUtil;
+import ru.skoltech.cedl.dataexchange.repository.DatabaseStorage;
 import ru.skoltech.cedl.dataexchange.structure.DummySystemBuilder;
 import ru.skoltech.cedl.dataexchange.structure.model.ModelNode;
 import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
@@ -15,16 +15,19 @@ public class DbStorageTest {
 
     private SystemModel systemModel;
 
+    private DatabaseStorage databaseStorage;
+
     @Before
     public void storeAndRetrieve() {
         systemModel = DummySystemBuilder.getSystemModel(4);
         System.out.println(systemModel);
-        DBUtil.storeModel(systemModel);
+        databaseStorage = new DatabaseStorage();
+        databaseStorage.storeModel(systemModel);
     }
 
     @Test
     public void compareStoredAndRetrievedModel() {
-        ModelNode modelNode = DBUtil.loadModel();
+        ModelNode modelNode = databaseStorage.loadModel();
         System.out.println(modelNode);
 
         Assert.assertEquals(modelNode.getName(), systemModel.getName());
