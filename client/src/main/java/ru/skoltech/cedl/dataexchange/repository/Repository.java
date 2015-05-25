132b20283888d54380d4fe835c1fa7483ad05fa5
Dominik Knoll <d.knoll@skoltech.ru>
2015-05-25 17:05:39 +0300

    added storing of UserManagement

--- client/src/main/java/ru/skoltech/cedl/dataexchange/repository/Repository.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/repository/Repository.java
@@ -2,13 +2,14 @@ package ru.skoltech.cedl.dataexchange.repository;
 
 import ru.skoltech.cedl.dataexchange.structure.model.Study;
 import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
+import ru.skoltech.cedl.dataexchange.users.model.UserManagement;
 
 /**
  * Created by D.Knoll on 25.05.2015.
  */
 public interface Repository {
 
-    SystemModel loadSystemModel();
+    SystemModel loadSystemModel(long studyId);
 
     void storeSystemModel(SystemModel systemModel);
 
@@ -17,4 +18,8 @@ public interface Repository {
     Study loadStudy(String name);
 
     void storeStudy(Study study);
+
+    void storeUserManagement(UserManagement userManagement);
+
+    UserManagement loadUserManagement(long studyId);
 }
