b044d054786da3a672435e4a020bdf6bfe32370f
Dominik Knoll <d.knoll@skoltech.ru>
2015-05-25 14:50:34 +0300

    introduced an interface and a factory

---
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/repository/Repository.java
@@ -0,0 +1,15 @@
+package ru.skoltech.cedl.dataexchange.repository;
+
+import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
+
+/**
+ * Created by D.Knoll on 25.05.2015.
+ */
+public interface Repository {
+
+    SystemModel loadSystemModel();
+
+    void storeSystemModel(SystemModel systemModel);
+
+    void close();
+}
--- client/src/test/java/ru/skoltech/cedl/dataexchange/DbStorageTest.java
+++ client/src/test/java/ru/skoltech/cedl/dataexchange/DbStorageTest.java
@@ -4,9 +4,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import ru.skoltech.cedl.dataexchange.repository.DatabaseStorage;
+import ru.skoltech.cedl.dataexchange.repository.RepositoryFactory;
 import ru.skoltech.cedl.dataexchange.structure.DummySystemBuilder;
 import ru.skoltech.cedl.dataexchange.structure.model.ModelNode;
 import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by dknoll on 23/05/15.
@@ -18,16 +22,18 @@ public class DbStorageTest {
     private DatabaseStorage databaseStorage;
 
     @Before
-    public void storeAndRetrieve() {
+    public void storeAndRetrieve() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         systemModel = DummySystemBuilder.getSystemModel(4);
         System.out.println(systemModel);
-        databaseStorage = new DatabaseStorage();
-        databaseStorage.storeModel(systemModel);
+        Constructor<DatabaseStorage> constructor = DatabaseStorage.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        databaseStorage = constructor.newInstance();
+        databaseStorage.storeSystemModel(systemModel);
     }
 
     @Test
     public void compareStoredAndRetrievedModel() {
-        ModelNode modelNode = databaseStorage.loadModel();
+        ModelNode modelNode = databaseStorage.loadSystemModel();
         System.out.println(modelNode);
 
         Assert.assertEquals(modelNode.getName(), systemModel.getName());
