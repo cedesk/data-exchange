7381b297f25f92573a70167c04f41d6b2314546c
Dominik Knoll <d.knoll@skoltech.ru>
2015-06-10 01:54:28 +0300

    added User name verification

--- client/src/main/java/ru/skoltech/cedl/dataexchange/Identifiers.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/Identifiers.java
@@ -6,17 +6,30 @@ import java.util.regex.Pattern;
  * Created by dknoll on 16/05/15.
  */
 public class Identifiers {
-    private static final Pattern NAME_RULE = Pattern.compile("[a-zA-Z]*");
+    private static final Pattern NAME_RULE = Pattern.compile("^[a-zA-Z]{1,}$");
+
+    private static final Pattern USER_NAME_RULE = Pattern.compile("^[a-zA-Z]{1,}[\\.\\-_]?[a-zA-Z]{1,}$");
 
     private static final String NAME_RULE_DESCRIPTION = "Names must be at least of 1 character and contain only alphanumeric characters!";
+
+    private static final String USER_NAME_RULE_DESCRIPTION = "User names must be at least of 1 character and contain only alphanumeric characters!";
 
     public static boolean validateNodeName(String nodeName) {
         if (nodeName.length() < 1) return false;
         return NAME_RULE.matcher(nodeName).matches();
     }
 
+    public static boolean validateUserName(String userName) {
+        if (userName.length() < 2) return false;
+        return USER_NAME_RULE.matcher(userName).matches();
+    }
+
     public static String getNameValidationDescription() {
         return NAME_RULE_DESCRIPTION;
+    }
+
+    public static String getUserNameValidationDescription() {
+        return USER_NAME_RULE_DESCRIPTION;
     }
 
 }
