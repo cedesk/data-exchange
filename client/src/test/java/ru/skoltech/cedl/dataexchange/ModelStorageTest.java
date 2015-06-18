8be33c914eaeec3e168a03aa757657aa2ab5622f
Dominik Knoll <d.knoll@skoltech.ru>
2015-06-18 23:28:47 +0300

    improved tests to verify storage

--- client/src/test/java/ru/skoltech/cedl/dataexchange/ModelStorageTest.java
+++ client/src/test/java/ru/skoltech/cedl/dataexchange/ModelStorageTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import ru.skoltech.cedl.dataexchange.repository.DatabaseStorage;
 import ru.skoltech.cedl.dataexchange.repository.RepositoryException;
 import ru.skoltech.cedl.dataexchange.structure.DummySystemBuilder;
+import ru.skoltech.cedl.dataexchange.structure.model.ParameterModel;
 import ru.skoltech.cedl.dataexchange.structure.model.SystemModel;
 
 import java.lang.reflect.Constructor;
@@ -48,4 +49,79 @@ public class ModelStorageTest {
         Assert.assertEquals(systemModel1, systemModel);
     }
 
+    @Test
+    public void storeModifyAndStore() throws RepositoryException {
+        SystemModel storedModel = DummySystemBuilder.getSystemModel(1);
+        System.out.println(storedModel);
+
+        databaseStorage.storeSystemModel(storedModel);
+        long systemModelId = storedModel.getId();
+
+        ParameterModel parameterModel = storedModel.getParameters().get(0);
+        int newValue = parameterModel.getValue().intValue() * 123;
+        parameterModel.setValue((double) newValue);
+
+        databaseStorage.storeSystemModel(storedModel);
+
+        SystemModel retrievedModel = databaseStorage.loadSystemModel(systemModelId);
+        ParameterModel parameterModel1 = retrievedModel.getParameters().get(0);
+        int storedValue = parameterModel1.getValue().intValue();
+
+        Assert.assertEquals(newValue, storedValue);
+    }
+
+    @Test
+    public void storeModifyAndStore2() throws RepositoryException {
+        SystemModel storedModel = DummySystemBuilder.getSystemModel(2);
+        System.out.println(storedModel);
+
+        SystemModel system0 = databaseStorage.storeSystemModel(storedModel);
+        long systemModelId = storedModel.getId();
+
+        ParameterModel parameterModel = storedModel.getParameters().get(0);
+        int newValue = parameterModel.getValue().intValue() * 123;
+        parameterModel.setValue((double) newValue);
+
+        SystemModel system1 = databaseStorage.storeSystemModel(storedModel);
+        Assert.assertEquals(system0, system1);
+
+        SystemModel retrievedModel = databaseStorage.loadSystemModel(systemModelId);
+        ParameterModel parameterModel1 = retrievedModel.getParameters().get(0);
+        int storedValue = parameterModel1.getValue().intValue();
+
+        Assert.assertEquals(newValue, storedValue);
+
+        SystemModel retrievedModel2 = databaseStorage.loadSystemModel(systemModelId);
+        ParameterModel parameterModel2 = retrievedModel2.getParameters().get(0);
+        newValue = parameterModel2.getValue().intValue() * 123;
+        parameterModel.setValue((double) newValue);
+        SystemModel system2 = databaseStorage.storeSystemModel(retrievedModel2);
+
+        Assert.assertEquals(system2, retrievedModel2);
+    }
+
+    @Test
+    public void storeModifyAndStoreNames() throws RepositoryException {
+        SystemModel storedModel = DummySystemBuilder.getSystemModel(2);
+        System.out.println(storedModel);
+
+        databaseStorage.storeSystemModel(storedModel);
+        long systemModelId = storedModel.getId();
+
+        ParameterModel parameterModel = storedModel.getParameters().get(0);
+        String newName = parameterModel.getName() + "1";
+        parameterModel.setName(newName);
+        int newValue = (int) (parameterModel.getValue().intValue() * 123);
+        parameterModel.setValue((double) newValue);
+
+        databaseStorage.storeSystemModel(storedModel);
+
+        SystemModel retrievedModel = databaseStorage.loadSystemModel(systemModelId);
+        ParameterModel parameterModel1 = retrievedModel.getParameters().get(0);
+        String retName = parameterModel1.getName();
+        int storedValue = parameterModel1.getValue().intValue();
+
+        Assert.assertEquals(newName, retName);
+        Assert.assertEquals(newValue, storedValue);
+    }
 }
