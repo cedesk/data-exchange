ef5faca5bc7b8ab1089da36f22d2c788e518502c
Dominik Knoll <d.knoll@skoltech.ru>
2015-06-19 02:22:06 +0300

    added nicer frontend represetation of Enum values

--- client/src/main/java/ru/skoltech/cedl/dataexchange/structure/model/ParameterType.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/structure/model/ParameterType.java
@@ -4,6 +4,14 @@ package ru.skoltech.cedl.dataexchange.structure.model;
  * Created by D.Knoll on 12.03.2015.
  */
 public enum ParameterType {
-    DefaultValue,
-    CalculatedValue
+    DefaultValue {
+        public String toString() {
+            return "manual";
+        }
+    },
+    CalculatedValue {
+        public String toString() {
+            return "calculated";
+        }
+    }
 }
