5e4e2b0ce5c22b568c37571faa420928deec7b3a
Dominik Knoll <d.knoll@skoltech.ru>
2015-07-03 00:03:02 +0300

    added Utils

--- client/src/main/java/ru/skoltech/cedl/dataexchange/structure/ExternalModel.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/structure/ExternalModel.java
@@ -1,8 +1,10 @@
 package ru.skoltech.cedl.dataexchange.structure;
 
 import org.hibernate.envers.Audited;
+import ru.skoltech.cedl.dataexchange.structure.model.ModificationTimestamped;
 
 import javax.persistence.*;
+import java.util.Arrays;
 
 /**
  * Created by D.Knoll on 02.07.2015.
@@ -10,11 +12,15 @@ import javax.persistence.*;
 @Entity
 @Access(AccessType.PROPERTY)
 @Audited
-public class ExternalModel {
+public class ExternalModel implements ModificationTimestamped {
 
-    protected long id;
+    private long id;
+
+    private String name;
 
     private byte[] attachment;
+
+    private Long lastModification;
 
     @Id
     @GeneratedValue
@@ -26,6 +32,14 @@ public class ExternalModel {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Column(length = 100 * 1024 * 1024)
     public byte[] getAttachment() {
         return attachment;
@@ -33,5 +47,34 @@ public class ExternalModel {
 
     public void setAttachment(byte[] attachment) {
         this.attachment = attachment;
+    }
+
+    @Override
+    public Long getLastModification() {
+        return lastModification;
+    }
+
+    @Override
+    public void setLastModification(Long lastModification) {
+        this.lastModification = lastModification;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ExternalModel that = (ExternalModel) o;
+
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        return Arrays.equals(attachment, that.attachment);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (attachment != null ? Arrays.hashCode(attachment) : 0);
+        return result;
     }
 }
