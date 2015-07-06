f1d7192b42e48e21dcbda3ba17d11e6070295697
Dominik Knoll <d.knoll@skoltech.ru>
2015-07-06 14:47:48 +0300

    added CoordinatesParser + Test

---
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/SpreadsheetCoordinates.java
@@ -0,0 +1,70 @@
+package ru.skoltech.cedl.dataexchange;
+
+import javafx.scene.control.TablePosition;
+
+import java.text.ParseException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by D.Knoll on 06.07.2015.
+ */
+public class SpreadsheetCoordinates {
+
+    public static final Pattern PATTERN = Pattern.compile("([A-Z]{1,})([1-9][0-9]*)");
+    private static final int INIT = (int) 'A';
+    private int rowNumber;
+    private int columnNumber;
+
+    public SpreadsheetCoordinates(int rowNumber, int columnNumber) {
+        this.rowNumber = rowNumber;
+        this.columnNumber = columnNumber;
+    }
+
+    public static String fromPosition(TablePosition tablePosition) {
+        return tablePosition.getTableColumn().getText() + String.valueOf(tablePosition.getRow() + 1);
+    }
+
+    public static SpreadsheetCoordinates valueOf(TablePosition tablePosition) {
+        return new SpreadsheetCoordinates(tablePosition.getRow(), tablePosition.getColumn());
+    }
+
+    public static SpreadsheetCoordinates valueOf(String coordinates) throws ParseException {
+        Matcher matcher = PATTERN.matcher(coordinates);
+        if (matcher.matches()) {
+            String row = matcher.group(2);
+            int rowNumber = Integer.valueOf(row);
+            String col = matcher.group(1);
+            int colNumber = 0;
+            for (char ch : col.toCharArray()) {
+                colNumber = colNumber * 27 + ch - INIT + 1;
+            }
+            return new SpreadsheetCoordinates(rowNumber, colNumber);
+        } else {
+            throw new ParseException("invalid coordinate format", 0);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        int col = columnNumber;
+        while (col > 26) {
+            int digit = (col % 27);
+            sb.append((char) (digit - 1 + INIT));
+            col = col / 27;
+        }
+        sb.append((char) (col - 1 + INIT));
+        sb = sb.reverse();
+        sb.append(rowNumber);
+        return sb.toString();
+    }
+
+    public int getRowNumber() {
+        return rowNumber;
+    }
+
+    public int getColumnNumber() {
+        return columnNumber;
+    }
+}
---
+++ client/src/test/java/ru/skoltech/cedl/dataexchange/CoordinateParserTest.java
@@ -0,0 +1,43 @@
+package ru.skoltech.cedl.dataexchange;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.text.ParseException;
+
+/**
+ * Created by D.Knoll on 06.07.2015.
+ */
+public class CoordinateParserTest {
+
+    @Test
+    public void convertFromString() throws ParseException {
+        SpreadsheetCoordinates a1 = SpreadsheetCoordinates.valueOf("A1");
+        Assert.assertEquals(1, a1.getRowNumber());
+        Assert.assertEquals(1, a1.getColumnNumber());
+
+        String d2_ = "D2";
+        SpreadsheetCoordinates d2 = SpreadsheetCoordinates.valueOf(d2_);
+        Assert.assertEquals(2, d2.getRowNumber());
+        Assert.assertEquals(4, d2.getColumnNumber());
+        Assert.assertEquals(d2_, d2.toString());
+
+        String z11_ = "Z11";
+        SpreadsheetCoordinates z11 = SpreadsheetCoordinates.valueOf(z11_);
+        Assert.assertEquals(11, z11.getRowNumber());
+        Assert.assertEquals(26, z11.getColumnNumber());
+        Assert.assertEquals(z11_, z11.toString());
+
+        String aa23_ = "AA23";
+        SpreadsheetCoordinates aa23 = SpreadsheetCoordinates.valueOf(aa23_);
+        Assert.assertEquals(23, aa23.getRowNumber());
+        Assert.assertEquals(28, aa23.getColumnNumber());
+        Assert.assertEquals(aa23_, aa23.toString());
+
+        String bz23_ = "BZ56";
+        SpreadsheetCoordinates bz23 = SpreadsheetCoordinates.valueOf(bz23_);
+        Assert.assertEquals(56, bz23.getRowNumber());
+        Assert.assertEquals(27 * 2 + 26, bz23.getColumnNumber());
+        Assert.assertEquals(bz23_, bz23.toString());
+    }
+}
