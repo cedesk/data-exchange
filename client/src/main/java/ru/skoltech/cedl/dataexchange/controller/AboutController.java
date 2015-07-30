3b07772fa76302bb2596e101bac044ad0b6e14a2
Dominik Knoll <d.knoll@skoltech.ru>
2015-07-30 16:58:26 +0300

    gui improvements

--- client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
@@ -2,6 +2,7 @@ package ru.skoltech.cedl.dataexchange.controller;
 
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.controlsfx.control.HyperlinkLabel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,14 +13,14 @@ import java.util.ResourceBundle;
 public class AboutController implements Initializable {
 
     public Label versionInfoLabel;
-    public Label authorsInfoLabel;
-    public Label librariesInfoLabel;
+    public HyperlinkLabel authorsInfoLabel;
+    public HyperlinkLabel librariesInfoLabel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         String version = AboutController.class.getPackage().getImplementationVersion();
         versionInfoLabel.setText(version);
-        authorsInfoLabel.setText("Dominik J. Knoll, d.knoll@skoltech.ru");
-        librariesInfoLabel.setText("Hibernate,\nMySQL,\nslf4j,\nApache POI,\n, Apache Commons Math 3,\nControlsFX - FX Experience,\nJFXtras");
+        authorsInfoLabel.setText("Dominik J. Knoll [d.knoll@skoltech.ru]");
+        librariesInfoLabel.setText("Hibernate,\nMySQL,\nslf4j,\nApache POI, Apache Commons Math 3,\nControlsFX - FX Experience,\nJFXtras");
     }
 }
