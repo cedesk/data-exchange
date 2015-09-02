00b874207acbc2d11a10eac7b415bcfc4114a267
Dominik Knoll <d.knoll@skoltech.ru>
2015-09-02 11:21:05 +0300

    revamped about screen

--- client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
@@ -1,8 +1,9 @@
 package ru.skoltech.cedl.dataexchange.controller;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import org.controlsfx.control.HyperlinkLabel;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,15 +13,13 @@ import java.util.ResourceBundle;
  */
 public class AboutController implements Initializable {
 
-    public Label versionInfoLabel;
-    public HyperlinkLabel authorsInfoLabel;
-    public HyperlinkLabel librariesInfoLabel;
+    @FXML
+    private WebView contentView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        String version = AboutController.class.getPackage().getImplementationVersion();
-        versionInfoLabel.setText(version);
-        authorsInfoLabel.setText("Dominik J. Knoll [d.knoll@skoltech.ru]");
-        librariesInfoLabel.setText("Hibernate,\nMySQL,\nslf4j,\nApache POI, Apache Commons Math 3,\nControlsFX - FX Experience,\nJFXtras");
+        URL url = getClass().getResource("/about.html");
+        WebEngine webEngine = contentView.getEngine();
+        webEngine.load(url.toExternalForm());
     }
 }
