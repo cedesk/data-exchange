9081d4a5685de431ab7f0a7d6f2ac135470b6eb4
Dominik Knoll <d.knoll@skoltech.ru>
2015-09-21 00:34:39 +0300

    changedlogging output no longer to go to app directory but to user-home

--- client/src/main/java/ru/skoltech/cedl/dataexchange/ClientApplication.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/ClientApplication.java
@@ -8,15 +8,26 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import ru.skoltech.cedl.dataexchange.controller.MainController;
 import ru.skoltech.cedl.dataexchange.structure.view.IconSet;
 import ru.skoltech.cedl.dataexchange.view.Views;
+
+import java.io.File;
 
 public class ClientApplication extends Application {
 
     private static Logger logger = Logger.getLogger(ClientApplication.class);
 
     public static void main(String[] args) {
+        if (System.getProperty("cedesk.data.dir") == null) {
+            System.setProperty("cedesk.data.dir", new File(System.getProperty("user.home"), "CEDESK").getAbsolutePath());
+        }
+        // ensure log directory exists
+        new File(System.getProperty("cedesk.data.dir")).mkdirs();
+        // now it's safe to configure log4j
+        PropertyConfigurator.configure(ClientApplication.class.getResource("/META-INF/log4j.properties"));
+
         logger.info("----------------------------------------------------------------------------------------------------");
         logger.info("Opening CEDESK ...");
         launch(args);
