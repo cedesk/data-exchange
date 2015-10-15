0be5c19f8a1681376cbf61762dfc9e1764de31d0
Dominik Knoll <d.knoll@skoltech.ru>
2015-10-15 16:47:33 +0300

    reworked loading of webpage

--- client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/controller/AboutController.java
@@ -1,10 +1,15 @@
 package ru.skoltech.cedl.dataexchange.controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import org.apache.log4j.Logger;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -13,13 +18,26 @@ import java.util.ResourceBundle;
  */
 public class AboutController implements Initializable {
 
+    private static final Logger logger = Logger.getLogger(AboutController.class);
+
     @FXML
     private WebView contentView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        URL url = getClass().getResource("/about.html");
+        URI uri = null;
+        try {
+            uri = getClass().getResource("/about.html").toURI();
+        } catch (URISyntaxException e) {
+            logger.error(e);
+        }
         WebEngine webEngine = contentView.getEngine();
-        webEngine.load(url.toExternalForm());
+        webEngine.getLoadWorker().exceptionProperty().addListener(new ChangeListener<Throwable>() {
+            @Override
+            public void changed(ObservableValue<? extends Throwable> observableValue, Throwable oldThrowable, Throwable newThrowable) {
+                logger.error("Load exception ", newThrowable);
+            }
+        });
+        webEngine.load(uri.toString());
     }
 }
