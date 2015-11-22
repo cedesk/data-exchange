9ceb7d16e4690a606370fc26c1885fe194c60445
Dominik Knoll <d.knoll@skoltech.ru>
2015-11-22 23:58:35 +0300

    changed Interceptor to work with hibernate 5

--- client/src/main/java/ru/skoltech/cedl/dataexchange/repository/ModificationInterceptor.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/repository/ModificationInterceptor.java
@@ -2,7 +2,6 @@ package ru.skoltech.cedl.dataexchange.repository;
 
 import org.apache.log4j.Logger;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.Transaction;
 import org.hibernate.type.Type;
 import ru.skoltech.cedl.dataexchange.structure.model.ExternalModel;
 import ru.skoltech.cedl.dataexchange.structure.model.ModelNode;
@@ -17,29 +16,21 @@ import java.io.Serializable;
 public class ModificationInterceptor extends EmptyInterceptor {
 
     private static Logger logger = Logger.getLogger(ModificationInterceptor.class);
-    private long flushTimeStamp;
-
-    @Override
-    public void afterTransactionBegin(Transaction tx) {
-        flushTimeStamp = System.currentTimeMillis(); // FIX: problem when clocks among clients are unsyncronized 
-    }
 
     @Override
     public boolean onFlushDirty(Object entity, Serializable id,
                                 Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
 
         if (entity instanceof ModificationTimestamped) {
-            logger.debug(entity.getClass().getCanonicalName() + "#" + id + ":" + getNodeName(currentState, propertyNames));
-            ModificationTimestamped timestampedEntity = (ModificationTimestamped) entity;
-            timestampedEntity.setLastModification(flushTimeStamp);
-            System.out.println("flushDirty - " + entity.getClass().getCanonicalName() + ".lastModification: " + timestampedEntity.getLastModification());
+            Long timestamp = updateTimestamp(currentState, propertyNames);
+            logger.debug("onFlushDirty " + entity.getClass().getCanonicalName() + "#" + id + ":" + getNodeName(currentState, propertyNames) + ", ts: " + timestamp);
         }
         return false;
     }
 
     @Override
     public boolean onSave(Object entity, Serializable id,
-                          Object[] state, String[] propertyNames, Type[] types) {
+                          Object[] currentState, String[] propertyNames, Type[] types) {
         if (entity instanceof ModificationTimestamped) {
             String nodeName = null;
             if (entity instanceof ModelNode) {
@@ -49,22 +40,31 @@ public class ModificationInterceptor extends EmptyInterceptor {
             } else if (entity instanceof ExternalModel) {
                 nodeName = ((ExternalModel) entity).getNodePath();
             } else {
-                nodeName = getNodeName(state, propertyNames);
+                nodeName = getNodeName(currentState, propertyNames);
             }
-            logger.debug(entity.getClass().getCanonicalName() + "#" + id + ":" + nodeName);
-            ModificationTimestamped timestampedEntity = (ModificationTimestamped) entity;
-            timestampedEntity.setLastModification(flushTimeStamp);
-            System.out.println("onSave - " + entity.getClass().getCanonicalName() + ".lastModification: " + timestampedEntity.getLastModification());
+            Long timestamp = updateTimestamp(currentState, propertyNames);
+            logger.debug("onSave " + entity.getClass().getCanonicalName() + "#" + id + ":" + nodeName + ", ts: " + timestamp);
         }
         return false;
     }
 
-    private String getNodeName(Object[] state, String[] propertyNames) {
+    private String getNodeName(Object[] currentState, String[] propertyNames) {
         for (int i = 0; i < propertyNames.length; i++) {
             if (propertyNames[i].equals("name")) {
-                return (String) state[i];
+                return (String) currentState[i];
             }
         }
         return null;
+    }
+
+    private Long updateTimestamp(Object[] currentState, String[] propertyNames) {
+        for (int i = 0; i < propertyNames.length; i++) {
+            if (propertyNames[i].equals("lastModification")) {
+                Long timestamp = System.currentTimeMillis();
+                currentState[i] = timestamp;
+                return timestamp;
+            }
+        }
+        return 0L;
     }
 }
