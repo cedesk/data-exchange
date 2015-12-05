c3b8e0c3c4e6663472e79d4198a3dcba0226fe1c
Dominik Knoll <d.knoll@skoltech.ru>
2015-12-05 11:45:48 +0300

    added possibility to update an existing view tree structure

--- client/src/main/java/ru/skoltech/cedl/dataexchange/structure/view/StructureTreeItemFactory.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/structure/view/StructureTreeItemFactory.java
@@ -1,113 +1,102 @@
 package ru.skoltech.cedl.dataexchange.structure.view;
 
-import javafx.scene.Group;
-import javafx.scene.Node;
-import javafx.scene.effect.BlendMode;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+import javafx.collections.ObservableList;
+import javafx.scene.control.TreeItem;
 import org.apache.log4j.Logger;
-import ru.skoltech.cedl.dataexchange.structure.model.*;
+import ru.skoltech.cedl.dataexchange.structure.model.CompositeModelNode;
+import ru.skoltech.cedl.dataexchange.structure.model.ModelNode;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Created by D.Knoll on 12.03.2015.
  */
 public class StructureTreeItemFactory {
 
-    public static final Image FLASH_OVERLAY = new Image("/icons/flash-ol.png");
     private static final Logger logger = Logger.getLogger(StructureTreeItemFactory.class);
-    private static final int ICON_SIZE = 24;
-    private static IconSet ICON_SET = new IconSet.Nodes();
 
     public static StructureTreeItem getTreeView(CompositeModelNode modelNode) {
-        StructureTreeItem node = getTreeNodeView(modelNode);
-        node.setExpanded(true);
+        StructureTreeItem viewNode = new StructureTreeItem(modelNode);
+        viewNode.setExpanded(true);
         for (ModelNode subNode : (List<ModelNode>) modelNode.getSubNodes()) {
             if (subNode instanceof CompositeModelNode) {
                 StructureTreeItem childNode = getTreeView((CompositeModelNode) subNode);
                 if (childNode != null) {
-                    node.getChildren().add(childNode);
+                    viewNode.getChildren().add(childNode);
                 }
             } else {
-                StructureTreeItem childNode = getTreeNodeView(subNode);
-                if (childNode != null) {
-                    node.getChildren().add(childNode);
-                }
+                StructureTreeItem childNode = new StructureTreeItem(subNode);
+                viewNode.getChildren().add(childNode);
             }
         }
-        return node;
+        return viewNode;
     }
 
     public static StructureTreeItem getTreeView(CompositeModelNode localModel, CompositeModelNode remoteModel) {
-        StructureTreeItem node = getTreeNodeView(localModel, remoteModel);
-        node.setExpanded(true);
+        StructureTreeItem viewNode = new StructureTreeItem(localModel, remoteModel);
+        viewNode.setExpanded(true);
         for (ModelNode subNode : (List<ModelNode>) localModel.getSubNodes()) {
             Map<String, ModelNode> remoteModelSubNodesMap = remoteModel.getSubNodesMap();
             ModelNode remoteSubNode = remoteModelSubNodesMap.get(subNode.getName());
             if (subNode instanceof CompositeModelNode) {
                 StructureTreeItem childNode = getTreeView((CompositeModelNode) subNode, (CompositeModelNode) remoteSubNode);
                 if (childNode != null) {
-                    node.getChildren().add(childNode);
+                    viewNode.getChildren().add(childNode);
                 }
             } else {
-                StructureTreeItem childNode = getTreeNodeView(subNode, remoteSubNode);
-                if (childNode != null) {
-                    node.getChildren().add(childNode);
+                StructureTreeItem childNode = new StructureTreeItem(subNode, remoteSubNode);
+                viewNode.getChildren().add(childNode);
+            }
+        }
+        return viewNode;
+    }
+
+    public static void updateTreeView(TreeItem<ModelNode> viewNode, ModelNode localModel, ModelNode remoteModel) {
+        StructureTreeItem structureTreeItem = (StructureTreeItem) viewNode;
+        if (localModel instanceof CompositeModelNode) {
+            Map<String, ModelNode> localModelSubNodesMap = getSubNodeDictionary((CompositeModelNode) localModel);
+            Map<String, ModelNode> remoteModelSubNodesMap = getSubNodeDictionary((CompositeModelNode) remoteModel);
+            ObservableList<TreeItem<ModelNode>> viewNodeChildren = viewNode.getChildren();
+            Map<String, TreeItem<ModelNode>> viewNodeMap = viewNodeChildren.stream()
+                    .collect(Collectors.toMap(modelNodeTreeItem -> modelNodeTreeItem.getValue().getUuid(), Function.identity()));
+
+            Set<String> uuids = new TreeSet<>();
+            uuids.addAll(viewNodeMap.keySet());
+            uuids.addAll(localModelSubNodesMap.keySet());
+            for (String uuid : uuids) {
+                TreeItem<ModelNode> childViewNode = viewNodeMap.get(uuid);
+                ModelNode localSubNode = localModelSubNodesMap.get(uuid);
+                ModelNode remoteSubNode = remoteModelSubNodesMap.get(uuid);
+                if (childViewNode != null && localSubNode == null) { // view node not in model
+                    viewNodeChildren.remove(childViewNode);
+                } else if (childViewNode == null && localSubNode != null) { // model node not in view
+                    if (localSubNode instanceof CompositeModelNode) {
+                        StructureTreeItem childNode = getTreeView((CompositeModelNode) localSubNode, (CompositeModelNode) remoteSubNode);
+                        if (childNode != null) {
+                            viewNodeChildren.add(childNode);
+                        }
+                    } else {
+                        StructureTreeItem childNode = new StructureTreeItem(localSubNode, remoteSubNode);
+                        viewNodeChildren.add(childNode);
+                    }
+                } else if (childViewNode != null && localSubNode != null) { // view node and model node present
+                    updateTreeView(childViewNode, localSubNode, remoteSubNode);
                 }
             }
         }
-        return node;
+        structureTreeItem.updateValues(localModel, remoteModel);
     }
 
-    public static StructureTreeItem getTreeNodeView(ModelNode model) {
-        StructureTreeItem structureTreeItem = new StructureTreeItem(model);
-        setGraphic(structureTreeItem, false);
-        return structureTreeItem;
-    }
-
-    public static StructureTreeItem getTreeNodeView(ModelNode local, ModelNode remote) {
-        StructureTreeItem structureTreeItem = new StructureTreeItem(local, remote);
-        setGraphic(structureTreeItem, structureTreeItem.hasChange());
-        return structureTreeItem;
-    }
-
-    private static void setGraphic(StructureTreeItem structureTreeItem, boolean overlayFlash) {
-        ModelNode model = structureTreeItem.getValue();
-        IconType iconType = getIconTypeForModel(model);
-        structureTreeItem.setGraphic(getImageView(iconType, overlayFlash));
-    }
-
-    private static IconType getIconTypeForModel(ModelNode model) {
-        if (model instanceof SystemModel) {
-            return IconType.SYSTEM;
-        } else if (model instanceof SubSystemModel) {
-            return IconType.SUBSYSTEM;
-        } else if (model instanceof ElementModel) {
-            return IconType.ELEMENT;
-        } else if (model instanceof InstrumentModel) {
-            return IconType.INSTRUMENT;
-        } else {
-            logger.error("UNKNOWN model encountered: " + model.getName() + " (" + model.getClass().getName() + "");
-            return null;
-        }
-    }
-
-    private static Node getImageView(IconType iconType, boolean overlayFlash) {
-        ImageView icon = new ImageView(ICON_SET.getIcon(iconType));
-        icon.setFitHeight(ICON_SIZE);
-        icon.setFitWidth(ICON_SIZE);
-
-        if (overlayFlash) {
-            ImageView overlay = new ImageView(FLASH_OVERLAY);
-            overlay.setFitHeight(ICON_SIZE);
-            overlay.setFitWidth(ICON_SIZE);
-            overlay.setBlendMode(BlendMode.SRC_OVER);
-
-            return new Group(icon, overlay);
-        } else {
-            return icon;
-        }
+    public static Map<String, ModelNode> getSubNodeDictionary(CompositeModelNode compositeModelNode) {
+        List<ModelNode> subNodes = compositeModelNode.getSubNodes();
+        Map<String, ModelNode> result = subNodes.stream().collect(
+                Collectors.toMap(ModelNode::getUuid, Function.identity())
+        );
+        return result;
     }
 }
