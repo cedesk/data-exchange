77895b4b7ae2aa8616bcab87e6a8dd2a13831564
Dominik Knoll <d.knoll@skoltech.ru>
2016-07-13 19:22:00 +0300

    added opening model in cache folder

--- client/src/main/java/ru/skoltech/cedl/dataexchange/control/ExternalModelView.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/control/ExternalModelView.java
@@ -4,9 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import org.apache.log4j.Logger;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -27,8 +29,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.*;
 import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -42,6 +44,9 @@ public class ExternalModelView extends HBox implements Initializable {
 
     @FXML
     private TextField externalModelNameText;
+
+    @FXML
+    private Button openExternalButton;
 
     public ExternalModelView(ExternalModel externalModel) {
         this.externalModel = externalModel;
@@ -73,24 +78,42 @@ public class ExternalModelView extends HBox implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         externalModelNameText.setText(externalModel.getName());
+        File filePathInCache = ExternalModelFileHandler.getFilePathInCache(externalModel);
+        File pathInCache = filePathInCache.getParentFile();
+        openExternalButton.addEventFilter(MouseEvent.MOUSE_PRESSED, (MouseEvent event) -> {
+            if (event.isSecondaryButtonDown())
+                try {
+                    openFile(pathInCache);
+                } catch (IOException ioe) {
+                    logger.error("Error saving external model to spreadsheet.", ioe);
+                } catch (Exception e) {
+                    logger.error("Error opening external model with default editor.", e);
+                }
+        });
     }
 
     public void openExternalModel(ActionEvent actionEvent) {
         ExternalModelFileHandler externalModelFileHandler = ProjectContext.getInstance().getProject().getExternalModelFileHandler();
         try {
             File file = externalModelFileHandler.cacheFile(externalModel);
-            if (file != null) {
-                Desktop desktop = Desktop.getDesktop();
-                if (desktop.isSupported(Desktop.Action.EDIT)) {
-                    desktop.edit(file);
-                } else {
-                    StatusLogger.getInstance().log("Unable to open file!", true);
-                }
-            }
+            openFile(file);
         } catch (IOException ioe) {
             logger.error("Error saving external model to spreadsheet.", ioe);
         } catch (Exception e) {
             logger.error("Error opening external model with default editor.", e);
+        }
+    }
+
+    private void openFile(File file) throws IOException {
+        if (file != null) {
+            Desktop desktop = Desktop.getDesktop();
+            if (file.isFile() && desktop.isSupported(Desktop.Action.EDIT)) {
+                desktop.edit(file);
+            } else if (file.isDirectory() && desktop.isSupported(Desktop.Action.BROWSE)) {
+                desktop.browse(file.toURI());
+            } else {
+                StatusLogger.getInstance().log("Unable to open file!", true);
+            }
         }
     }
 
