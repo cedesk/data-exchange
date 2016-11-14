7cd4a2280269eda93a0309a6fa9f44a81bf0211a
Dominik Knoll <d.knoll@skoltech.ru>
2016-11-14 21:36:51 +0300

    made a new control for a Diagram Viewer

--- client/src/test/java/ru/skoltech/cedl/dataexchange/DiagramGraphicsTest.java
+++ client/src/test/java/ru/skoltech/cedl/dataexchange/DiagramGraphicsTest.java
@@ -6,71 +6,52 @@ package ru.skoltech.cedl.dataexchange;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
+import ru.skoltech.cedl.dataexchange.control.DiagramViewer;
 
 public class DiagramGraphicsTest extends Application {
+
     public static void main(String[] args) {
         Application.launch(args);
     }
 
     @Override
     public void start(Stage stage) {
-        Circle circle = new Circle(40);
-        circle.setFill(Color.RED);
-        circle.setStroke(Color.BLACK);
-        circle.setStrokeWidth(2.0);
 
-        Rectangle rect = new Rectangle(120, 75);
-        rect.setFill(Color.RED);
+        DiagramViewer diagramViewer = new DiagramViewer();
+        diagramViewer.setMinWidth(600);
+        diagramViewer.setMinHeight(400);
 
-        // Create a line
-        Line line = new Line(0, 0, 150, 50);
-        line.setStrokeWidth(5.0);
-        line.setStroke(Color.GREEN);
+        Scene scene = new Scene(diagramViewer);
+        stage.setScene(scene);
+        stage.setTitle("Diagram Viewer Test");
+        stage.show();
 
-        // Create a parallelogram
-        Polygon parallelogram = new Polygon();
-        parallelogram.getPoints().addAll(30.0, 0.0,
-                130.0, 0.0,
-                120.00, 50.0,
-                0.0, 50.0);
-        parallelogram.setFill(Color.AZURE);
-        parallelogram.setStroke(Color.BLACK);
+        diagramViewer.addElement("Element 1");
+        diagramViewer.addElement("Element 2");
+        diagramViewer.addConnection("Element 1", "Element 2", "param. A,\nparam. Z");
 
-        // Create a hexagon
-        Polyline hexagon = new Polyline(100.0, 0.0,
-                120.0, 20.0,
-                110.0, 140.0,
-                100.0, 60.0,
-                80.0, 40.0,
-                80.0, 120.0,
-                100.0, 0.0);
-        hexagon.setFill(Color.WHITE);
-        hexagon.setStroke(Color.BLACK);
+        diagramViewer.addElement("Element 3");
+        diagramViewer.addConnection("Element 1", "Element 3", "parameter B");
+        diagramViewer.addConnection("Element 3", "Element 2", "parameter C");
 
+        diagramViewer.addElement("Element 4");
+        diagramViewer.addConnection("Element 3", "Element 4", "param. Y");
+        diagramViewer.addConnection("Element 4", "Element 2", "param. X");
+
+    }
+
+
+    private Arc getArc() {
         // A CHORD arc with no fill and a stroke
         Arc arc = new Arc(0, 0, 50, 100, 0, 90);
         arc.setFill(Color.TRANSPARENT);
         arc.setStroke(Color.BLACK);
         arc.setType(ArcType.CHORD);
-
-        // Add all shapes to an HBox
-        HBox root =
-                new HBox(circle, rect, line, parallelogram, hexagon, arc);
-        root.setSpacing(10);
-        root.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.setTitle("2D Shapes");
-        stage.show();
+        return arc;
     }
+
 }
