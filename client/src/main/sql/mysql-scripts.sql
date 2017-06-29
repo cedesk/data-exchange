bcb84b19d236cc53209fae1c6cc29897595a9ad4
Dominik Knoll <d.knoll@skoltech.ru>
2017-06-29 16:59:41 +0300

    limited permissions

--- client/src/main/sql/mysql-scripts.sql
+++ client/src/main/sql/mysql-scripts.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA `cedesk_repo` DEFAULT CHARACTER SET utf8 ;
-
+CREATE SCHEMA `cedesk_repo` DEFAULT CHARACTER SET utf8;
 CREATE USER 'cedesk'@'%' IDENTIFIED BY 'cedesk';
-
-GRANT ALL PRIVILEGES ON * . * TO 'cedesk'@'%';
+GRANT ALL PRIVILEGES ON 'cedesk_repo'. * TO 'cedesk'@'%';
