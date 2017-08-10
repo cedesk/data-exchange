d478a4310e3827ef52feaa54ced46d18f83d460e
Dominik Knoll <d.knoll@skoltech.ru>
2017-08-10 19:09:05 +0200

    added menu item for opening the tradespace explorer

--- client/src/main/java/ru/skoltech/cedl/dataexchange/controller/MainController.java
+++ client/src/main/java/ru/skoltech/cedl/dataexchange/controller/MainController.java
@@ -487,6 +487,10 @@ public class MainController implements Initializable {
         guiService.openView("Change History Analyis", Views.CHANGE_HISTORY_ANALYSIS_WINDOW, getAppWindow());
     }
 
+    public void openTradespaceExplorer() {
+        guiService.openView("Tradespace Explorer", Views.TRADESPACE_WINDOW, getAppWindow());
+    }
+
     public void openDepencencyView() {
         guiService.openView("N-Square Chart", Views.DEPENDENCY_WINDOW, getAppWindow());
     }
