fcb5478fd0b41650856188d4410eae72c1525ed2
n.groshkov <n.groshkov@skoltech.ru>
2017-10-25 14:11:31 +0300

    fix #160 resolve script has been added.

--- client/src/main/sql/fix-project-creation.sql
+++ client/src/main/sql/fix-project-creation.sql
@@ -6,5 +6,3 @@ UPDATE study_aud set lastModification = latestModelModification WHERE lastModifi
 
 ALTER TABLE study DROP COLUMN latestModelModification;
 ALTER TABLE study_aud DROP COLUMN latestModelModification;
-
-DELETE FROM  hibernate_sequence WHERE next_val = 51;
