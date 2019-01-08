0e7da931887c92f75f20bd13b2494ee1315d07a9
Dominik Knoll <d.knoll@skoltech.ru>
2019-01-09 00:41:01 +0300

    removed reminances

--- client/src/main/sql/fix-project-creation.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE externalmodel DROP COLUMN version; 
-ALTER TABLE parametermodel DROP COLUMN version; 
-
-UPDATE study set lastModification = latestModelModification WHERE lastModification IS NULL;
-UPDATE study_aud set lastModification = latestModelModification WHERE lastModification IS NULL;
-
-ALTER TABLE study DROP COLUMN latestModelModification;
-ALTER TABLE study_aud DROP COLUMN latestModelModification;
